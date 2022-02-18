7423e48181a584a77dfd58e438440ec7e5e034fa
Afzalbek <uzafo.dev@gmail.com>
2022-02-18 22:48:03 +0500

    Edited router

--- src/router/router.js
+++ src/router/router.js
@@ -7,12 +7,12 @@ import Portfolio from '@/pages/components/Portfolio.vue';
 import Login from '@/pages/Login.vue';
 
 const routes = [
-  { path: '/', component: Main },
-  { path: '/about-me', component: Aboutme },
-  { path: '/contact', component: Contact },
-  { path: '/blog', component: Blog },
-  { path: '/portfolio', component: Portfolio },
-  { path: '/login', component: Login },
+  { path: '/:text', component: Main},
+  { path: '/about-me/:text', component: Aboutme, alias: '/about-me' },
+  { path: '/contact/:text', component: Contact },
+  { path: '/blog/:text', component: Blog },
+  { path: '/portfolio/:text', component: Portfolio },
+  { path: '/login/:text', component: Login },
 ]
 
 const router = createRouter({
